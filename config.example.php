fb6893af09bdf0cfd067e68aea894b5065fe3423
Yeri Tiete <yeri@tiete.be>
2012-07-18 00:56:42 +0200

    go.flatturtle.com

---
+++ config.example.php
@@ -0,0 +1,34 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Template folder
+|--------------------------------------------------------------------------
+|
+| The folder name of the default template
+*/
+
+/*
+| -------------------------------------------------------------------
+| Database connectivity settings
+| -------------------------------------------------------------------
+| The database settings needed to access your database.
+|
+|	['hostname'] The hostname of your database server.
+|	['username'] The username used to connect to the database
+|	['password'] The password used to connect to the database
+|	['database'] The name of the database you want to connect to
+|	['port'] 	 The port used to connect to the database
+*/
+
+$dbhost = 'localhost';
+$dbuser = 'lulz';
+$dbpass = 'blablobli';
+$db = 'yeri';
+
+$config['database']['hostname'] = $dbhost;
+$config['database']['username'] = $dbuser;
+$config['database']['password'] = $dbpass;
+$config['database']['database'] = $db;
+$config['database']['port'] = '';
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This uses RedBeanPHP to get a variable from our database
+ * When the right parameter has been passed as a hostname,
+ * it will redirect to the right infoscreen
+ */
+include_once("config.php");
+include_once("rb.php");
+
+//get the hostname
+$g = array_keys($_GET);
+$strs = "";
+if(isset($g[0])){
+	$strs = $g[0];
+}
+$stra = explode("/",$strs);
+$hostname = "";
+if(isset($stra[1])){
+	$hostname = $stra[1];
+}
+
+//Talk to the database
+if(isset($hostname) && $hostname != ""){
+	R::setup("mysql:host=localhost;dbname=$db",$dbuser,$dbpass);
+	$haystack = $hostname;
+	$needles = R::find('infoscreens',' hostname = ?', array( $haystack ));
+	foreach($needles as $needle){
+		$infoscreen = $needle["alias"];
+		header("location: http://s.flatturtle.com/stable/" . $infoscreen);
+	}
+	if(sizeof($needles) == 0){
+		header("location: http://s.flatturtle.com/stable/");
+	}
+}else{
+	header("location: http://s.flatturtle.com/stable/");
+}
+
+?>
